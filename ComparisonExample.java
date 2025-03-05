dc504228d97baf4f2bf12696c7509e812f26c971
ANAND KUMAR <ak3173055@gmail.com>
2025-03-05 11:08:31 +0530

    Added examples for logical AND and OR operators in NotOperatorExample.java

---
+++ ComparisonExample.java
@@ -0,0 +1,24 @@
+public class ComparisonExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        // Equal to
+        System.out.println("x == y: " + (x == y)); // false
+
+        // Not equal to
+        System.out.println("x != y: " + (x != y)); // true
+
+        // Greater than
+        System.out.println("x > y: " + (x > y)); // false
+
+        // Less than
+        System.out.println("x < y: " + (x < y)); // true
+
+        // Greater than or equal to
+        System.out.println("x >= y: " + (x >= y)); // false
+
+        // Less than or equal to
+        System.out.println("x <= y: " + (x <= y)); // true
+    }
+}
